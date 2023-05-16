f5ad59dcdf2b56a7cf557c35a149c6d655b923bd
Juan Felipe Castaño <jf.castanol@uniandes.edu.co>
2023-05-16 09:26:25 -0500

    final

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import messages_es from "./locales/es.json";
+import messages_en from "./locales/en.json";
 import JobsList from "./components/jobslist";
+import { IntlProvider } from "react-intl";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+const messages = {
+  es: messages_es,
+  en: messages_en,
+};
+
+const getLanguage = () => {
+  const userLang = navigator.language || navigator.userLanguage;
+  const language = userLang.split(/[-_]/)[0];
+  return language;
+};
+
+const language = getLanguage();
+
+ReactDOM.render(
+  <IntlProvider locale={language} messages={messages[language]}>
+    <JobsList language={language} />
+  </IntlProvider>,
+  document.getElementById("root")
+);
